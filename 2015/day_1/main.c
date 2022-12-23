3f656725568e3d37bd823b6e34e5578a340f95d6
Thomas Joly <thomas.joly@ledger.fr>
2022-12-23 17:26:36 +0100

    new format 2015 day 1

---
+++ 2015/day_1/main.c
@@ -0,0 +1,37 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/queue.h>
+
+typedef struct input {
+  char *str;
+} input_t;
+
+//#include "sample.h"
+#include "puzzle.h"
+
+int main(void) {
+  int floor = 0;
+  const char *str = inputs[0].str;
+  bool part_2 = false;
+
+  for (unsigned i = 0; i < strlen(str); ++i) {
+    if (str[i] == '(') {
+      ++floor;
+    } else if (str[i] == ')') {
+      --floor;
+    } else {
+      abort();
+    }
+
+    if (part_2 == false && floor == -1) {
+      printf("> part 2: %u\n", i + 1);
+      part_2 = true;
+    }
+  }
+
+  printf("> part 1: %u\n", floor);
+  return 0;
+}
