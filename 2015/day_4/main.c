4b2bb35c6fe17cf66d0c220a2d355bc76cbc08a2
Thomas Joly <thomas.joly@ledger.fr>
2022-12-24 11:19:02 +0100

    day_4 2015 update

---
+++ 2015/day_4/main.c
@@ -0,0 +1,60 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/queue.h>
+
+#include <openssl/md5.h>
+
+typedef struct input {
+  char *str;
+} input_t;
+
+//#include "sample.h"
+#include "puzzle.h"
+
+static bool start_with_0(unsigned char *result, unsigned nr_zero) {
+  if (nr_zero == 5) {
+    return result[0] == 0 && result[1] == 0 && (result[2] & 0xf0) == 0;
+  }
+  if (nr_zero == 6) {
+    return result[0] == 0 && result[1] == 0 && result[2] == 0;
+  }
+  abort();
+  return false;
+}
+
+#define INPUT_SZ 256
+
+static unsigned find_md5_start_with_0(const char *prefix, unsigned nr_zero) {
+  char input[INPUT_SZ];
+  unsigned n = 1;
+  unsigned char result[MD5_DIGEST_LENGTH];
+
+  while (n != 0) {
+    unsigned len = sprintf(input, "%s%u", prefix, n);
+    assert(len < INPUT_SZ);
+    MD5((unsigned char *)input, len, result);
+    if (start_with_0(result, nr_zero) == true) {
+      return n;
+    }
+    ++n;
+  }
+  abort(); // overflow
+  return 0;
+}
+
+int main(void) {
+  unsigned n;
+
+  // part 1
+  n = find_md5_start_with_0(inputs[0].str, 5);
+  printf("> part 1: %u\n", n);
+
+  // part 2
+  n = find_md5_start_with_0(inputs[0].str, 6);
+  printf("> part 2: %u\n", n);
+
+  return 0;
+}
--- README.md
+++ README.md
@@ -7,12 +7,6 @@
  + Day 1: [Not Quite Lisp](https://adventofcode.com/2015/day/1)
  + Day 2: [I Was Told There Would Be No Math](https://adventofcode.com/2015/day/2)
  + Day 3: [Perfectly Spherical Houses in a Vacuum](https://adventofcode.com/2015/day/3)
+ + Day 4: [The Ideal Stocking Stuffer](https://adventofcode.com/2015/day/4)
  + Day 13: [Knights of the Dinner Table](https://adventofcode.com/2015/day/13)
-
-### 2018\
  
- + Day 1 [**]: [Chronal Calibration](https://adventofcode.com/2018/day/1)
- + Day 2 [**]: [Inventory Management System](https://adventofcode.com/2018/day/2)
- + Day 3 [**]: [No Matter How You Slice It](https://adventofcode.com/2018/day/3)
-
-
