64273e607ed82768081d354e6faccff057256c34
Thomas Joly <thomas.joly@ledger.fr>
2022-12-24 09:34:49 +0100

    template

---
+++ template/main.c
@@ -0,0 +1,16 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/queue.h>
+
+typedef struct input {
+} input_t;
+
+#include "sample.h"
+//#include "puzzle.h"
+
+int main(void) {
+  return 0;
+}
