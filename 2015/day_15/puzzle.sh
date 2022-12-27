175ef93e7b10560c084f512cc46946c012e6f8d6
Thomas Joly <thomas.joly@ledger.fr>
2022-12-27 18:15:43 +0100

    2015 day 15 generate combination

--- 2015/day_15/puzzle.sh
+++ 2015/day_15/puzzle.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 INPUT="$1"
+
+TARGET=100
 
 cat <<EOF
 static const input_t inputs[] = {
@@ -24,6 +26,62 @@ cat <<EOF
 };
 
 #define NR_INPUT $nr_input
+
+const int combis[][NR_INPUT] = {
+EOF
+
+if [ $nr_input -eq 2 ]
+then
+  n=0
+  for a in $(seq 0 $TARGET)
+  do
+    for b in $(seq 0 $TARGET)
+      do
+        if [ $((a + b)) -eq $TARGET ]
+        then
+          if [ $((n % 4 )) -eq 0 ]
+          then
+            echo -n "  { $a, $b },"
+          else
+            echo -n " { $a, $b },"
+            if [ $((n % 4)) -eq 3 ]
+            then
+              echo ""
+            fi
+          fi
+          n=$((n + 1))
+        fi
+    done
+  done
+elif [ $nr_input -eq 4 ]
+then
+  n=0
+  for a in $(seq 0 $TARGET)
+  do
+    for b in $(seq 0 $TARGET)
+    do
+      [ $((a + b)) -gt $TARGET ] && continue
+      for c in $(seq 0 $TARGET)
+      do
+        [ $((a + b + c)) -gt $TARGET ] && continue
+        d=$(($TARGET - a - b - c))
+        if [ $((n % 4 )) -eq 0 ]
+        then
+          echo -n "  { $a, $b, $c, $d },"
+        else
+          echo -n " { $a, $b, $c, $d },"
+          [ $((n % 4)) -eq 3 ] && echo ""
+        fi
+        n=$((n + 1))
+      done
+    done
+  done
+fi
+
+cat <<EOF
+};
+
+#define NR_COMBI $n
 EOF
 
 exit 0
