4c2cfa48c92e3839caf8c8b0c2863b8c8ac499c4
Thomas Joly <thomas.joly@ledger.fr>
2023-01-11 16:22:11 +0100

    2016 day 2

---
+++ 2016/day_2/puzzle.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+INPUT="$1"
+
+cat <<EOF
+static const input_t inputs[] = {
+EOF
+
+nr_input=0
+
+while read -r line
+do
+  echo "{\"$line\"},"
+  nr_input=$((nr_input + 1))
+done < $INPUT
+
+cat <<EOF
+};
+
+#define NR_INPUT $nr_input
+EOF
+
+exit 0
